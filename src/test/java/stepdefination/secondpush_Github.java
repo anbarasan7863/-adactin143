be21dcba28e381bd3c4f0f4bcd559b7539f107c3
anbarasan7863 <anbarasan7863@gmail.com>
2023-01-26 12:00:10 +0530

    second push

---
+++ src/test/java/stepdefination/secondpush_Github.java
@@ -0,0 +1,10 @@
+package stepdefination;
+
+public class secondpush_Github {
+	public static void gitpush() {
+		System.out.println("second push");
+	}
+public static void main(String[] args) {
+	gitpush();
+}
+}
