0d25b9dc048ef9bda573f89db59134d0f7514a8b
anbarasan7863 <anbarasan7863@gmail.com>
2023-01-26 12:07:11 +0530

    git pull

--- src/test/java/stepdefination/secondpush_Github.java
+++ src/test/java/stepdefination/secondpush_Github.java
@@ -4,7 +4,11 @@ public class secondpush_Github {
 	public static void gitpush() {
 		System.out.println("second push");
 	}
+	public static void gitpull() {
+		System.out.println("gitpull");
+	}
 public static void main(String[] args) {
 	gitpush();
+	gitpull();
 }
 }
